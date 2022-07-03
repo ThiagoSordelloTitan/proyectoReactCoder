1fb5de9b2cf696c9faa707448b46138d2c687f55
Thiago Sordello Titan <thiagosordellotitan@gmail.com>
2022-07-03 19:54:24 -0300

    contador

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import ItemListContainer from './components/ItemCount/ItemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <body>
+      <ItemListContainer greeting={"Contador"} />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "../ItemCount/ItemCount.css"
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+
+    const [count, setCount] = useState(initial)
+
+    const sumar = () => {
+        if (stock > count) {
+            setCount(count + 1)
+        }
+    }
+
+    const restar = () => {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+    }
+
+    const agregarCantidad = () => {
+        onAdd(count)
+    }
+
+    return (
+        <>
+            <div className="containerContador">
+                <div className="containerButton">
+                    <button className="boton1" onClick={restar}> - </button>
+
+                    <label>{count}</label>
+
+                    <button className="boton1" onClick={sumar}> + </button>
+                </div>
+                <button className="agregarCantidad" onClick={agregarCantidad}> Agregar al carrito </button>
+            </div>
+        </>
+    )
+}
+
+
+export default ItemCount
+
