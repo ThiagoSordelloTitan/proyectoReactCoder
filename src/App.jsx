3983382de63bc6ff7c0d99ed7399be982dd33813
Thiago Sordello Titan <thiagosordellotitan@gmail.com>
2022-07-05 00:01:23 -0300

    se agrega itemDetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header>
       </header>
       <body>
-      {/* <ItemListContainer greeting={"Contador"} /> */}
+      <ItemListContainer greeting={"Contador"} />
       <ItemDetailContainer />
 
       </body>
