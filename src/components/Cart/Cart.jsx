a9df4e1dc7df1db846a4af08bd9f2b759c06f466
Thiago Sordello Titan <thiagosordellotitan@gmail.com>
2022-07-26 21:48:51 -0300

    orden de compra

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import { useCartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import ItemCart from "../ItemCart/ItemCart";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
 
 
 const Cart = () => {
 
     const { cart, PrecioTotal } = useCartContext();
+
+    const Orden = {
+        comprador: {
+            nombre: "thiago",
+            email: "thiagoreact@gmail.com",
+            teléfono: "123456789",
+            dirección: "callefalsa 123"
+        },
+        productos: cart.map(producto => ( {id: producto.id, titulo: producto.id, precio: producto.id, cantidad: producto.cantidad })) ,
+        total: PrecioTotal(),
+    }
+
+    const RealizarCompra = () => {
+        const db = getFirestore();
+        const ColeccionOrdenes = collection(db, "ordenes");
+        addDoc(ColeccionOrdenes, Orden)
+        .then (( {id} ) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -23,6 +43,7 @@ const Cart = () => {
         <>
             {cart.map(producto => <ItemCart key={producto.id} producto={producto} />)}
             <p>Total: {PrecioTotal()} </p>
+            <button onClick={RealizarCompra}> Realizar Compra </button>
         </>
     )
 }
